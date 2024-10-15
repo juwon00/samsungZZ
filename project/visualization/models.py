44c7150f6e5c7324b85d2329f7b2e4928239400d
SGEBRM <tkdalsdl330@hanmail.net>
2024-10-15 18:43:54 +0900

    feat : 편의시설 정보 제공

--- project/visualization/models.py
+++ project/visualization/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Amenities(models.Model):
+    station_name = models.CharField(max_length=100)  # 역명
+    line_number = models.CharField(max_length=20)    # 호선
+    cultural_space = models.BooleanField(default=False)  # 문화공간 여부
+    wheelchair_lift = models.BooleanField(default=False) # 휠체어 리프트 여부
+    meeting_spot = models.BooleanField(default=False)    # 만남의 장소 여부
+    parking_lot = models.BooleanField(default=False)     # 환승 주차장 여부
+    bike_rack = models.BooleanField(default=False)       # 자전거 보관소 여부
+    elevator = models.BooleanField(default=False)        # 엘리베이터 여부
+    ticket_office = models.BooleanField(default=False)   # 기차 예매 여부
+    civil_service = models.BooleanField(default=False) # 무인 민원 발급기 여부
+    currency_exchange = models.BooleanField(default=False)      # 환전 키오스크 여부
+    nursing_room = models.BooleanField(default=False)           # 수유실 여부
+
+    def __str__(self):
+        return f"{self.station_name} ({self.line_number})"
