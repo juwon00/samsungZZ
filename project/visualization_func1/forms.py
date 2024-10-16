60dfa94388aa7cddb99b96c6cf287fea2fe7daf3
Hyerim Jeon <jshsn0806@naver.com>
2024-10-17 01:26:48 +0900

    Create forms.py
    정보를 필터링해줄 드롭 다운 필드 정의

---
+++ project/visualization_func1/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import DegreeOfSubwayCongestion
+
+# 드롭다운 필드 정의
+class FilterForm(forms.Form):
+    route_choices = DegreeOfSubwayCongestion.objects.values_list('route_name', flat=True).distinct()
+    time_choices = DegreeOfSubwayCongestion.objects.values_list('time', flat=True).distinct()
+
+    route_name = forms.ChoiceField(choices=[(route, route) for route in route_choices], label="호선 선택")
+    weekday = forms.ChoiceField(choices=[(1, "평일"), (2, "주말")], label="평일/주말")
+    time = forms.ChoiceField(choices=[(time, time) for time in time_choices], label="시간대 선택")
