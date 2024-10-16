9f8b75e9bf172ada9659f48dd343cde859df7cf4
SGEBRM <tkdalsdl330@hanmail.net>
2024-10-16 17:22:52 +0900

    refactor : 시각화 결합
    그래프와 편의 시설 시각화 코드 결합

--- project/visualization/forms.py
+++ project/visualization/forms.py
@@ -1,5 +1,24 @@
 from django import forms
 
-class StationSearchForm(forms.Form):
-    station_name = forms.CharField(label='Station Name', max_length=100)
-    line_number = forms.CharField(label='Line Number', max_length=20)
+#class StationSearchForm(forms.Form):
+    
+
+class SubwayDataForm(forms.Form):
+    # line 필드는 드롭다운 형식
+    LINE_CHOICES = [
+        ('1호선', '1호선'),
+        ('2호선', '2호선'),
+        ('3호선', '3호선'),
+        ('4호선', '4호선'),
+        ('5호선', '5호선'),
+        ('6호선', '6호선'),
+        ('7호선', '7호선'),
+        ('8호선', '8호선'),
+    ]
+    line = forms.ChoiceField(label='호선', choices=LINE_CHOICES)
+
+    # 역이름은 문자열 입력
+    sttn = forms.CharField(label='지하철역', max_length=20)
+
+    #station_name = forms.CharField(label='Station Name', max_length=100)
+    #line_number = forms.CharField(label='Line Number', max_length=20)
--- project/visualization/urls.py
+++ project/visualization/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.station_search, name='station_search'),
+    #path('search/', views.station_search, name='station_search'),
+    path('graph/', views.subway_passenger_graph, name='subway_passenger_graph'),
 ]
