847abf365a2e885e335e411b73d5b20491d0ca72
juone00 <juwon0208@naver.com>
2024-10-17 14:35:21 +0900

    feat: 역별 위도 경도 데이터 저장 및 조회 기능 추가

--- project/data_collection/views.py
+++ project/data_collection/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import SubwayMonthlyTimeSlotPassengerCounts, DegreeOfSubwayCongestion, SubwayDailyTimeSlotPassengerDifference, SubwayAmenities
-from .serializers import SubwayPassengerCountSerializer, DegreeOfSubwayCongestionSerializer, SubwayPassengerDifferenceSerializer, SubwayAmenitiesSerializer
+from .models import (
+    SubwayMonthlyTimeSlotPassengerCounts,
+    DegreeOfSubwayCongestion,
+    SubwayDailyTimeSlotPassengerDifference,
+    SubwayAmenities,
+    SubwayStationLatLng,
+)
+from .serializers import (
+    SubwayPassengerCountSerializer,
+    DegreeOfSubwayCongestionSerializer,
+    SubwayPassengerDifferenceSerializer,
+    SubwayAmenitiesSerializer,
+    SubwayStationLatLngSerializer,
+)
 
 
 class SubwayMonthlyPassengerCounterListView(APIView):
@@ -25,6 +37,7 @@ class SubwayMonthlyPassengerCounterListView(APIView):
 
         return Response(serializer.data[0])
 
+
 class DegreeOfSubwayCongestionListView(APIView):
     def get(self, request, *args, **kwargs):
         week = request.query_params.get("week", None)
@@ -39,7 +52,7 @@ class DegreeOfSubwayCongestionListView(APIView):
             queryset = queryset.filter(week=week)
         if time:
             queryset = queryset.filter(time=time)
-        
+
         serializer = DegreeOfSubwayCongestionSerializer(queryset, many=True)
 
         return Response(serializer.data)
@@ -49,15 +62,13 @@ class SubwayDailyPassengerDifferenceView(APIView):
     def get(self, request, date, line, sttn, time_slot):
         try:
             data = SubwayDailyTimeSlotPassengerDifference.objects.get(
-                date=date,
-                line=line,
-                sttn=sttn,
-                time_slot=time_slot
+                date=date, line=line, sttn=sttn, time_slot=time_slot
             )
             serializer = SubwayPassengerDifferenceSerializer(data)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except SubwayDailyTimeSlotPassengerDifference.DoesNotExist:
-            return Response({"detail":"Not Found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"detail": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+
 
 class SubwayAmenitiesView(APIView):
     def get(self, request, line, sttn):
@@ -69,4 +80,18 @@ class SubwayAmenitiesView(APIView):
             serializer = SubwayAmenitiesSerializer(data)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except SubwayAmenities.DoesNotExist:
-            return Response({"detail":"Not Found"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"detail": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class SubwayStationLatLngListView(APIView):
+    def get(self, request, *args, **kwargs):
+        route_name = request.query_params.get("route_name", None)
+
+        queryset = SubwayStationLatLng.objects.all()
+
+        if route_name:
+            queryset = queryset.filter(route_name=route_name)
+
+        serializer = SubwayStationLatLngSerializer(queryset, many=True)
+
+        return Response(serializer.data)
