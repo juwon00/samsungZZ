8d326b5417d0f588fa2556b2b4283bdfeba22c63
Hyerim Jeon <jshsn0806@naver.com>
2024-10-18 01:44:21 +0900

    DB에 저장된 형식에 맞춰 filtering

--- project/visualization_func1/forms.py
+++ project/visualization_func1/forms.py
@@ -4,8 +4,9 @@ from data_collection.models import DegreeOfSubwayCongestion
 # 드롭다운 필드 정의
 class FilterForm(forms.Form):
     route_choices = DegreeOfSubwayCongestion.objects.values_list('route_name', flat=True).distinct()
+    week_choices = DegreeOfSubwayCongestion.objects.values_list('week', flat=True).distinct()
     time_choices = DegreeOfSubwayCongestion.objects.values_list('time', flat=True).distinct()
 
     route_name = forms.ChoiceField(choices=[(route, route) for route in route_choices], label="호선 선택")
-    week = forms.ChoiceField(choices=[(1, "평일"), (2, "주말")], label="평일/주말")
+    week = forms.ChoiceField(choices=[("평일", "평일"), ("주말", "토요일" or "일요일")], label="평일/주말")
     time = forms.ChoiceField(choices=[(time, time) for time in time_choices], label="시간대 선택")
